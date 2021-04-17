511a7a9fbbcf3bb907f59f13581e74e624e05ee8
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2021-04-17 12:57:11 +0530

    git commit

---
+++ narcissistic-number-checker.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+void main()
+{
+    int num, rem, i, temp = 0;
+    int cub_num = 0;
+    int quo;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("\n");
+    temp = num;
+    while (temp>=10)
+    {
+        rem = temp % 10;
+        printf("Remainder value: %d\n", rem);
+        quo = temp / 10;
+        printf("Quotient value: %d\n", quo);
+        cub_num += rem*rem*rem*rem;
+        printf("%d^4 value: %d\n", rem, cub_num);
+        temp = quo;
+    }
+    cub_num += temp*temp*temp*temp;
+    printf("%d^4 value: %d\n\n", temp, cub_num);
+    if (cub_num == num)
+        printf("%d == %d, %d is a Narcissistic number", num, cub_num, num);
+    else
+        printf("%d != %d, %d is not a Narcissistic number", num, cub_num, num);
+}
