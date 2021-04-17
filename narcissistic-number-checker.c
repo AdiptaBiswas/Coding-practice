42db9eaf8a15e2b9c1ca310b238ecfdbb80393f9
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2021-04-17 13:00:40 +0530

    update code

--- narcissistic-number-checker.c
+++ narcissistic-number-checker.c
@@ -9,6 +9,7 @@ void main()
     scanf("%d", &num);
     printf("\n");
     temp = num;
+    
     while (temp>=10)
     {
         rem = temp % 10;
@@ -19,10 +20,32 @@ void main()
         printf("%d^4 value: %d\n", rem, cub_num);
         temp = quo;
     }
+    
     cub_num += temp*temp*temp*temp;
     printf("%d^4 value: %d\n\n", temp, cub_num);
+    
     if (cub_num == num)
         printf("%d == %d, %d is a Narcissistic number", num, cub_num, num);
+    
     else
         printf("%d != %d, %d is not a Narcissistic number", num, cub_num, num);
 }
+
+/* Output:
+
+Enter a number: 9474                                                                                                                                                           
+                                                                                                                                                                               
+Remainder value: 4                                                                                                                                                             
+Quotient value: 947                                                                                                                                                            
+4^4 value: 256                                                                                                                                                                 
+Remainder value: 7                                                                                                                                                             
+Quotient value: 94                                                                                                                                                             
+7^4 value: 2657                                                                                                                                                                
+Remainder value: 4                                                                                                                                                             
+Quotient value: 9                                                                                                                                                              
+4^4 value: 2913                                                                                                                                                                
+9^4 value: 9474                                                                                                                                                                
+                                                                                                                                                                               
+9474 == 9474, 9474 is a Narcissistic number
+
+*/
