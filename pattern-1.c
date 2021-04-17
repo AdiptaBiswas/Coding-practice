e43e79924274d9328d115d0040322894f5b4a05f
Adipta Biswas <40853486+AdiptaBiswas@users.noreply.github.com>
2021-04-17 09:39:10 +0530

    commit

---
+++ pattern-1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void pr_star(int);
+
+void main()
+{
+  int star = 10;
+  pr_star(star);
+}
+
+void pr_star(int star)
+{
+    printf("\t\t");
+    for (int i=1; i<=star/2; i++)
+    {
+        for (int j=1; j<=i; j++)
+        {
+            printf("%d", j);
+        }
+        for (int k=i; k<=star-i; k++)
+        {
+            printf(" ");
+        }
+        for (int l=(star-i)+1; l<=star; l++)
+        {
+            printf("%d", l);
+        }
+        printf("\n");
+        printf("\t\t");
+    }
+}
